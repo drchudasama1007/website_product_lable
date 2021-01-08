675d6d44ac338f2589f01c076b1f14cc0a43f5d1
Dharmesh <drchudasama644@gmail.com>
2021-01-08 11:09:30 +0530

    [ADD]code added for module change 12

--- website_product_lable/models/product_label.py
+++ website_product_lable/models/product_label.py
@@ -4,8 +4,8 @@ class ProductLabel(models.Model):
     _name = "product.label"
 
     name = fields.Char("Label Name")
-    bg_color = fields.Char("Background Color")
-    font_color = fields.Char("Font Color")
+    bg_color = fields.Char("HTML Background Color(eg, #FF0000)")
+    font_color = fields.Char("HTML Font Color(eg, #000000)")
 
 
 class ProductTemplate(models.Model):
